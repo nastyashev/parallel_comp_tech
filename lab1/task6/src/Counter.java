8b97f799ddd8234cc48bb5c235c7e90f0443f5b0
nastyashev <nastya.sova2004@gmail.com>
2024-03-04 20:42:10 +0200

    Add task 6

--- lab1/task6/src/Counter.java
+++ lab1/task6/src/Counter.java
@@ -1,5 +1,6 @@
 public class Counter {
     private int count = 0;
+    private Object lock = new Object();
 
     public void increment() {
         count++;
@@ -11,5 +12,41 @@ public class Counter {
 
     public int getCount() {
         return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void synchronizedIncrementLock() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+
+    public void synchronizedDecrementLock() {
+        synchronized (lock) {
+            count--;
+        }
+    }
+
+    public void synchronizedIncrementBlock() {
+        synchronized (this) {
+            count++;
+        }
+    }
+
+    public void synchronizedDecrementBlock() {
+        synchronized (this) {
+            count--;
+        }
+    }
+
+    public synchronized void synchronizedIncrementMethod() {
+        count++;
+    }
+
+    public synchronized void synchronizedDecrementMethod() {
+        count--;
     }
 }
--- lab1/task6/src/Main.java
+++ lab1/task6/src/Main.java
@@ -7,23 +7,85 @@ public class Main {
                 counter.increment();
             }
         });
-
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 100000; i++) {
                 counter.decrement();
             }
         });
-
         t1.start();
         t2.start();
-
         try {
             t1.join();
             t2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.out.println("Count: " + counter.getCount());
+
+        counter.setCount(0);
+
+        Thread t3 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                counter.synchronizedIncrementMethod();
+            }
+        });
+        Thread t4 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                counter.synchronizedDecrementMethod();
+            }
+        });
+        t3.start();
+        t4.start();
+        try {
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Count (synchronised method): " + counter.getCount());
+
+        counter.setCount(0);
+
+        Thread t5 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                counter.synchronizedIncrementBlock();
+            }
+        });
+        Thread t6 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                counter.synchronizedDecrementBlock();
+            }
+        });
+        t5.start();
+        t6.start();
+        try {
+            t5.join();
+            t6.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Count (synchronised block): " + counter.getCount());
+
+        counter.setCount(0);
+
+        Thread t7 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                counter.synchronizedIncrementLock();
+            }
+        });
+        Thread t8 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                counter.synchronizedDecrementLock();
+            }
+        });
+        t7.start();
+        t8.start();
+        try {
+            t7.join();
+            t8.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Count (blocking an object): " + counter.getCount());
     }
 }
