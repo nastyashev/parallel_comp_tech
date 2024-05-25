94f7921d937a46c54015d5a2afc7581149aca6ac
nastyashev <nastya.sova2004@gmail.com>
2024-05-25 13:33:47 +0300

    init

--- lab3/task3.2/src/Main.java
+++ lab3/task3.2/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
     }
 }
