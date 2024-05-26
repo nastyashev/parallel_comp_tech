dc759ce12b46b41c0a4ebb9b4aad7fa3c99760fd
Prosto-Vlad <vladdeynega8@gmail.com>
2024-05-26 03:37:19 +0300

    init

--- lab6/task6.2/src/Main.java
+++ lab6/task6.2/src/Main.java
@@ -1,4 +1,5 @@
-import mpi.*;
+import mpi.MPI;
+
 import static java.lang.System.exit;
 
 class Main {
@@ -16,6 +17,7 @@ class Main {
     private static final int NRC = NRA;
     private static final int NCC = NCB;
     private static final int N_SEND = 128;
+    private static int N_CALC;
 
     public static void main(String[] args) {
         a = new double[NRA][NCA];
@@ -26,15 +28,16 @@ class Main {
         int size = MPI.COMM_WORLD.Size();
         int rank = MPI.COMM_WORLD.Rank();
 
+        Main.N_CALC = Math.ceilDiv(NRC * NCC, size - 1);
+
         if (size < 2) {
-            System.out.println("Need at least two MPI tasks. Quitting...\\n");
+            System.out.println("Need at least two MPI tasks. Quitting...");
             MPI.COMM_WORLD.Abort(13);
             exit(13);
         }
 
-//        size = size - 1;
         if (rank == MASTER) {
-            System.out.format("mpi_mm has started with %d tasks.%n", size);
+            System.out.format("mpi_mm has started with %d tasks.%n", size - 1);
 
             for (int i = 0; i < NRA; i++) {
                 for (int j = 0; j < NCA; j++) {
@@ -47,116 +50,107 @@ class Main {
                 }
             }
 
-
-//            int averow = NRA / size;
-//            int extra = NRA % size;
-//            int offset = 0;
             for (int dest = 1; dest < size; dest++) {
-
+                int len = Math.min(N_CALC, NRC * NCC - (dest - 1) * N_CALC);
+                sendInt(len, dest, FROM_MASTER);
+                int offset = (dest - 1) * N_CALC;
+                sendInt(offset, dest, FROM_MASTER);
+                sendMatrix(a, dest, FROM_MASTER);
+                sendMatrix(b, dest, FROM_MASTER);
             }
+
+            for (int source = 1; source < size; source++) {
+                int offset = receiveInt(source, FROM_WORKER);
+                int len = receiveInt(source, FROM_WORKER);
+                double[][] cBlock = receiveMatrix(source, NRC, NCC, FROM_WORKER);
+
+                for (int i = 0; i < len; i++) {
+                    int row = Math.floorDiv(offset, NCC);
+                    int col = offset % NCC;
+                    c[row][col] = cBlock[i][0];
+                }
+            }
+
+            System.out.println("****");
+            System.out.println("Result Matrix:");
+            for (int i = 0; i < NRA; i++) {
+                System.out.println();
+                for (int j = 0; j < NCB; j++) {
+                    System.out.format("%6.2f ", c[i][j]);
+                }
+            }
+            System.out.println("\n****");
+            System.out.println("Done.");
+        } else {
+            int len = receiveInt(MASTER, FROM_MASTER);
+            int offset = receiveInt(MASTER, FROM_MASTER);
+            double[][] a = receiveMatrix(MASTER, NRA, NCA, FROM_MASTER);
+            double[][] b = receiveMatrix(MASTER, NRB, NCB, FROM_MASTER);
+
+            for (int i = 0; i < len; i++) {
+                int row = (offset + i) / NCC;
+                int col = (offset + i) % NCC;
+                c[row][col] = 0;
+                for (int j = 0; j < NCA; j++) {
+                    c[row][col] += a[row][j] * b[j][col];
+                }
+            }
+
+            sendInt(offset, MASTER, FROM_WORKER);
+            sendInt(len, MASTER, FROM_WORKER);
+            sendMatrix(c, MASTER, FROM_WORKER);
+
         }
-//            for (int dest = 1; dest <= size; dest++) {
-//                int rows = (dest <= extra) ? averow + 1 : averow;
-//                System.out.format("Sending %d rows to task %d offset=%d%n", rows, dest, offset);
-//                int[] tmp = new int[1]; // Java MPI використовує масиви
-//                tmp[0] = offset;
-//                MPI.COMM_WORLD.Send(tmp, 0, 1, MPI.INT, dest, FROM_MASTER);
-//                tmp[0] = rows; // Java MPI використовує масиви
-//                MPI.COMM_WORLD.Send(tmp, 0, 1, MPI.INT, dest, FROM_MASTER);
-//                for (int i = 0; i < NRA; i++) {
-//                    MPI.COMM_WORLD.Send(a[i], 0, NCA, MPI.DOUBLE, dest, FROM_MASTER);
-//                }
-//                double[] bData = new double[NCA * NCB];
-//                for (int i = 0; i < NCA; i++) {
-//                    MPI.COMM_WORLD.Send(b[i], 0, NCB, MPI.DOUBLE, dest, FROM_MASTER);
-//                }
-//                offset = offset + rows;
-//            }
-//            int[] data = new int[1]; // Оголошення масиву для отримання даних
-//
-//            for (int source = 1; source <= size; source++) {
-//                int[] tmp = new int[1];
-//                MPI.COMM_WORLD.Recv(tmp, 0, 1, MPI.INT, source, FROM_WORKER);
-//                offset = tmp[0];
-//                MPI.COMM_WORLD.Recv(tmp, 0, 1, MPI.INT, source, FROM_WORKER);
-//                int rows = tmp[0];
-//                double[][] cBlock = new double[rows][NCB]; // Оголошення двовимірного масиву для отримання блоку даних
-//                for (int i = 0; i < rows; i++) {
-//                    MPI.COMM_WORLD.Recv(cBlock[i], 0, NCB, MPI.DOUBLE, source, FROM_WORKER);
-//                }
-//                System.out.format("Received results from task %d%n", source);
-//            }
-//            System.out.println("****");
-//            System.out.println("Result Matrix:");
-//            for (int i = 0; i < NRA; i++) {
-//                System.out.println();
-//                for (int j = 0; j < NCB; j++) {
-//                    System.out.format("%6.2f ", c[i][j]);
-//                }
-//            }
-//            System.out.println("\n****");
-//            System.out.println("Done.");
-//        } else {
-//            int[] tmp = new int[1];
-//            MPI.COMM_WORLD.Recv(tmp, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-//            int offset = tmp[0];
-//            MPI.COMM_WORLD.Recv(tmp, 0, 1, MPI.INT, MASTER, FROM_MASTER);
-//            int rows = tmp[0];
-//
-//            for (int i = 0; i < NRA; i++) {
-//                MPI.COMM_WORLD.Recv(a[i], 0, NCA, MPI.DOUBLE, MASTER, FROM_MASTER);
-//            }
-//
-//
-//            double[] bData = new double[NCA * NCB];
-//            MPI.COMM_WORLD.Recv(bData, 0, NCA * NCB, MPI.DOUBLE, MASTER, FROM_MASTER);
-//            double[][] b = new double[NCA][NCB];
-//            int index = 0;
-//            for (int i = 0; i < NCA; i++) {
-//                for (int j = 0; j < NCB; j++) {
-//                    b[i][j] = bData[index++];
-//                }
-//            }
-//
-//            for (int i = 0; i < NCB; i++) {
-//                for (int j = 0; j < rows; j++) {
-//                    c[j][i] = 0.0;
-//                    for (int k = 0; k < NCA; k++) {
-//                        c[j][i] += a[j][k] * b[k][i];
-//                    }
-//                }
-//            }
-//
-//            tmp[0] = offset; // Оголошення масиву для передачі одного значення
-//            MPI.COMM_WORLD.Send(tmp, 0, 1, MPI.INT, MASTER, FROM_WORKER);
-//            tmp[0] = rows; // Оголошення масиву для передачі одного значення
-//            MPI.COMM_WORLD.Send(tmp, 0, 1, MPI.INT, MASTER, FROM_WORKER);
-//            double[] cData = new double[rows * NCB];
-//            index = 0;
-//            for (int i = 0; i < rows; i++) {
-//                for (int j = 0; j < NCB; j++) {
-//                    cData[index++] = c[i][j];
-//                }
-//            }
-//        }
 
         MPI.Finalize();
     }
 
-    public static void sendMatrix(double[][] matrix, int dest) {
+    private static void sendMatrix(double[][] matrix, int dest, int from) {
         int rows = matrix.length;
         int cols = matrix[0].length;
         double[] tmp = new double[rows * cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                tmp[i * cols + j] = matrix[i][j];
+                tmp[i * rows + j] = matrix[i][j];
             }
         }
 
         int nCountSend = Math.ceilDiv(rows * cols, N_SEND);
         for (int i = 0; i < nCountSend; i++) {
             int nSend = Math.min(N_SEND, rows * cols - i * N_SEND);
-            MPI.COMM_WORLD.Send(tmp, i * N_SEND, nSend, MPI.DOUBLE, dest, FROM_MASTER);
+//            System.out.println("Send: " + tmp + " " + i * N_SEND + " " + nSend + " " + MPI.DOUBLE + " " + dest + " " + from);
+            MPI.COMM_WORLD.Send(tmp, i * N_SEND, nSend, MPI.DOUBLE, dest, from);
         }
+    }
+
+    private static double[][] receiveMatrix(int source, int rows, int cols, int from) {
+        double[][] matrix = new double[rows][cols];
+        double[] tmp = new double[rows * cols];
+        int nCountRecv = Math.ceilDiv(rows * cols, N_SEND);
+        for (int i = 0; i < nCountRecv; i++) {
+            int nRecv = Math.min(N_SEND, rows * cols - i * N_SEND);
+//            System.out.println("Recv: " + tmp + " " + i * N_SEND + " " + nRecv + " " + MPI.DOUBLE + " " + source + " " + from + " " + (rows + " " + cols + " " + i + " " + N_SEND));
+            MPI.COMM_WORLD.Recv(tmp, i * N_SEND, nRecv, MPI.DOUBLE, source, from);
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = tmp[i * cols + j];
+            }
+        }
+
+        return matrix;
+    }
+
+    private static void sendInt(int value, int dest, int from) {
+        int[] tmp = new int[1];
+        tmp[0] = value;
+        MPI.COMM_WORLD.Send(tmp, 0, 1, MPI.INT, dest, from);
+    }
+
+    private static int receiveInt(int source, int from) {
+        int[] tmp = new int[1];
+        MPI.COMM_WORLD.Recv(tmp, 0, 1, MPI.INT, source, from);
+        return tmp[0];
     }
 }
